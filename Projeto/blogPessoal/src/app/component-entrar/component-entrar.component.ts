6b09400063fbe386202055da31d74b50aa1a820b
DaniloMendesF <danilmendes@gmail.com>
2021-02-17 12:12:47 -0300

    Implementação dos components entrar e cadastrar

---
+++ Projeto/blogPessoal/src/app/component-entrar/component-entrar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-entrar',
+  templateUrl: './component-entrar.component.html',
+  styleUrls: ['./component-entrar.component.css']
+})
+export class ComponentEntrarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
